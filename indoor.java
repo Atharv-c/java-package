63d854c7723425965787b80929c67599d2ba0ee6
Atharv-c <atharvtambe2002@gmail.com>
2026-01-17 15:43:46 +0530

    Add files via upload

---
+++ indoor.java
@@ -0,0 +1,3 @@
+/* Slip 6A) Write package for games in java which have
+ 2 classes indoor and outdoor  use function display() to generate list of players for specific games.
+ use parameterised constructor , finanlize method and array of object */
